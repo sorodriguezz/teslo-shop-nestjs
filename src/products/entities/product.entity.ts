8aa450332d28a510153db0dfd0e68266363a7530
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-08 11:24:11 -0300

    (feat) Se crea entity para product

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+
+  @Column('numeric', {
+    default: 0
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  size: string[];
+
+  @Column('text')
+  gender: string;
+}
