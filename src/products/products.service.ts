71b3a360404f9b0b46ae8d7b3968a7e92c796ba1
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-09 09:59:57 -0300

    (feat) Se crea dto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Product } from './entities/product.entity';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import {
   BadRequestException,
   InternalServerErrorException,
 } from '@nestjs/common/exceptions';
-import { NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
