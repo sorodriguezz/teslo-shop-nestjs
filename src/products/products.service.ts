53546b094e4f3f54cd295fd669ea81327e2f4edb
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-09 20:42:55 -0300

    (feat) Se agrega query builder para buscar por title y slug

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -49,7 +49,13 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('title=:title or slug=:slug', {
+          title: term,
+          slug: term,
+        })
+        .getOne();
     }
 
     if (!product) {
