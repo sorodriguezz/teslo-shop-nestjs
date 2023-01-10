d10a5ad2afe877a3377805f5ed486883283a5504
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-09 21:00:48 -0300

    (feat) Se hace update con preload

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -24,8 +34,11 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -65,8 +65,17 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+
+    //* preload busca un producto por id y carga el dto para formar un Product
+    const product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto,
+    });
+
+    if(!product) throw new NotFoundException(`Product with id ${ id } not found`);
+
+    return this.productRepository.save(product);
   }
 
   async remove(id: string) {
