da44e209a1f731134eab444de5d9b352a61a6b81
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-14 20:13:23 -0300

    (feat) Se añaden prodcutos con seed

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -10,6 +10,6 @@ import { Product, ProductImage } from './entities';
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [TypeOrmModule.forFeature([Product, ProductImage])],
-  exports: [ProductsService],
+  exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { ProductsService } from 'src/products/products.service';
+import { ProductsService } from './../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
 
   constructor(
-    private readonly productService: ProductsService,
-  ) {
+    private readonly productsService: ProductsService,
+  ) {}
 
-  }
-  async runSeed() {
+  async runSeed(): Promise<string> {
     await this.insertNewProduct();
     return 'SEED EXECUTED';
   }
 
   private async insertNewProduct(): Promise<boolean> {
-    await this.productService.deleteAllProducts();
+    await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises = [];
+
+    products.forEach( product => {
+      insertPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
+
     return true;
   }
 
