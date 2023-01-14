cf733f49672cb2a26bc2e9309007f13b93a9474d
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-14 19:39:10 -0300

    (feat) Se agrega queryRunner a update

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/common/exceptions';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { validate as isUUID } from 'uuid';
 
 import { CreateProductDto } from './dto/create-product.dto';
@@ -22,6 +22,7 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
     @InjectRepository(ProductImage)
     private readonly productImageRespository: Repository<ProductImage>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
@@ -95,18 +96,45 @@ export class ProductsService {
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
+    const { images, ...toUpdate } = updateProductDto;
+
     //* preload busca un producto por id y carga el dto para formar un Product
     const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
+      id,
+      ...toUpdate,
     });
 
     if (!product)
       throw new NotFoundException(`Product with id ${id} not found`);
+
+    // Create query runner
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      return await this.productRepository.save(product);
+      // si existen imagenes se borran
+      if (images) {
+        // si no va el segundo parametro corresponde a un DELETE * FROM
+        await queryRunner.manager.delete(ProductImage, {
+          product: { id },
+        });
+        product.images = images.map((image) =>
+          this.productImageRespository.create({ url: image }),
+        );
+      } //else {
+        //product.images = await this.productImageRespository.findBy({ product: { id }});
+      //}
+
+      await queryRunner.manager.save(product); // los query runner con manager no afecta en la bd hasta que se haga commit
+
+      await queryRunner.commitTransaction(); // se commitea
+      await queryRunner.release(); // se desconecta queryRunner
+
+      return this.findOnePlain(id);
     } catch (error) {
+      await queryRunner.rollbackTransaction(); // rollback del queryrunner
+      await queryRunner.release();
       this.handleDBExceptions(error);
     }
   }
