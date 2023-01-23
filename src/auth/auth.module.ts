0e90553eea54d44c6fdb779785503e73c6413d98
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-22 21:45:51 -0300

    (feat) Se configuran firmas de jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,32 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategyL: 'jwt' }),
+    JwtModule.registerAsync({
+      // registro asincrono de jwt
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // inyeccion de dependecias
+        return {
+          // se puede ocupar variable de entorno igual
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
