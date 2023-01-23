a9f6ab6c17f552e987f09480ca2e7ebc0587823e
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-01-22 21:05:54 -0300

    (feat) Se maneja errores con key duplicadas

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create.user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>, // para user una entity en el servicio
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      // preparar la data para insertar | crea una entity User de acuerdo al deo
+      const user = this.userRepository.create(createUserDto);
+
+      await this.userRepository.save(user);
+
+      return user;
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+  // jamar regresa un valor con never
+  private handleDBErrors( error: any ): never {
+    if(error.code === '23505'){
+      throw new BadRequestException(error.detail);
+    }
+
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs');
+
   }
 }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -16,7 +16,9 @@ export class User {
   @Column('text')
   fullName: string;
 
-  @Column('bool')
+  @Column('bool', {
+    default: true
+  })
   isActive: boolean;
 
   @Column('text', {
