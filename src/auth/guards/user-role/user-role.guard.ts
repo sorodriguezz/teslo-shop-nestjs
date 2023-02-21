4e82de68176cd7973ee6fc9155d58d24b8d0a58c
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-02-21 00:04:43 -0300

    (feat) Se crea custom decorator

---
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+
+  constructor(
+    private readonly reflector: Reflector,
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[] = this.reflector.get('roles', context.getHandler());
+
+    console.log({validRoles});
+
+    return true;
+  }
+}
+ 
