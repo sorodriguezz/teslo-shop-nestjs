74c069cfb62d5a743a7082bafbca799cea381a3f
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-02-26 16:31:00 -0300

    (feat) Se reconoce conteo de clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -8,11 +8,15 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
-    console.log('cliente conectado: ', client.id);
+    // console.log('cliente conectado: ', client.id);
+    this.messagesWsService.registerClient(client);
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
 
   handleDisconnect(client: any) {
-    console.log('cliente desconectado: ', client.id);
+    // console.log('cliente desconectado: ', client.id);
+    this.messagesWsService.removeClient(client.id);
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
 
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common/decorators';
+import { Socket } from 'socket.io';
+
+interface ConnectedClients {
+  [id: string]: Socket
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+
+  private connectedClients: ConnectedClients = {};
+
+  registerClient(client: Socket) {
+    this.connectedClients[client.id] = client;
+  }
+
+  removeClient(clientId: string) {
+    delete this.connectedClients[clientId];
+  }
+
+  getConnectedClients(): number {
+    return Object.keys(this.connectedClients).length;
+  }
+}
