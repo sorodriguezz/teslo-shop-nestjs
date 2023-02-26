69cb06bf936df4e0418dcc9451c6071748c02f84
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-02-26 16:03:24 -0300

    (feat) Se instala socke.io y se configura socket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,9 +1,18 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true }) // configuracion cors para socket
-export class MessagesWsGateway {
+@WebSocketGateway({ cors: true, namespace: '/' }) // configuracion cors para socket
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect { // para ver quien se conectó y desconectó
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
-  
+  handleConnection(client: Socket) {
+    console.log('cliente conectado: ', client.id);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('cliente desconectado: ', client.id);
+  }
+
 }
